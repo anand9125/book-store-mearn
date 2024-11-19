c4248e175465fed777b3d5398ef1a13d510a03a7
Anand <achaudharyskn@gmail.com>
2024-11-19 13:14:10 +0530

    cart change

--- frontend/src/Recoil/Cart/atom.jsx
+++ frontend/src/Recoil/Cart/atom.jsx
@@ -2,5 +2,5 @@ import { atom,  } from "recoil";
 
 export const cartState = atom({
    key: "cartAtom",
-   default : [0]
+   default : []
 });
--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -5,7 +5,7 @@
 // accessible by any component in the app.
 
 import { createContext, useContext, useEffect, useState } from "react";
-import { auth } from "../firebase/firebaseCOnfig.js";
+import {auth} from "../firebase/firebaseConfig"
 import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
 import { getAuth, signInWithPopup, GoogleAuthProvider,signOut,onAuthStateChanged } from "firebase/auth";
 const googleProvider = new GoogleAuthProvider();
